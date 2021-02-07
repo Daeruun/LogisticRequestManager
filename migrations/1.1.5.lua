dfba3f9ee884a5a2aed0a85efe24f50742bf7a25
Daeruun <zentgrafcn@gmail.com>
2021-02-07 20:43:18 +0100

    Update migration
    migration should ensure saved location is corrected

--- migrations/1.1.5.lua
+++ migrations/1.1.5.lua
@@ -6,4 +6,7 @@ for _, player in pairs(game.players) do
     if player.gui.screen["logistic-request-manager-gui-import-frame"] then
         player.gui.screen["logistic-request-manager-gui-import-frame"].destroy()
     end
+
+    -- make sure an empty location is nil
+    if not next(global["screen_location"][player.index]) then global["screen_location"][player.index] = nil end
 end
