816e4932c1e96fa463a27445432bcce4a07f90a5
Daeruun <zentgrafcn@gmail.com>
2021-02-11 02:49:52 +0100

    added sprites

--- data.lua
+++ data.lua
@@ -10,3 +10,29 @@ local button_toggle={
     consuming = "none"
 }
 data:extend{button_toggle}
+
+data:extend(
+{
+    {
+        type = "sprite",
+        name = "LRM-paste",
+        filename = "__base__/graphics/icons/shortcut-toolbar/mip/paste-x24.png",
+        priority = "extra-high-no-scale",
+        width = 24,
+        height = 24,
+        flags = {"gui-icon"},
+        mipmap_count = 2,
+        scale = 0.5
+      },
+      {
+        type = "sprite",
+        name = "LRM-copy",
+        filename = "__base__/graphics/icons/shortcut-toolbar/mip/copy-x24.png",
+        priority = "extra-high-no-scale",
+        width = 24,
+        height = 24,
+        flags = {"gui-icon"},
+        mipmap_count = 2,
+        scale = 0.5
+      },
+})
