5e3e99ab9c3c55565c7f80a9212bd484947f9862
Daeruun <zentgrafcn@gmail.com>
2021-02-24 22:22:31 +0100

    new hotkey to close gui: linked to toggle-menu

--- data.lua
+++ data.lua
@@ -2,14 +2,22 @@ require 'defines'
 
 require 'prototypes/style'
 
-
-local button_toggle={
+data:extend(
+{
+  {
     type = "custom-input",
     name = "LRM-input-toggle-gui",
     key_sequence = "SHIFT + L",
     consuming = "none"
-}
-data:extend{button_toggle}
+  },
+  {
+    type = "custom-input",
+    name = "LRM-input-close-gui",
+    key_sequence = "",
+    linked_game_control = "toggle-menu",
+    consuming = "none"
+  }
+})
 
 data:extend(
 {
