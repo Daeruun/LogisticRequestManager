d344293e3028ac6768d548531762e52623f17df6
Daeruun <zentgrafcn@gmail.com>
2021-02-24 21:52:20 +0100

    refactoring
    one common table as base for everything: lrm
    defines are now in lrm.defines

--- defines.lua
+++ defines.lua
@@ -1,64 +1,64 @@
-if not lrm then lrm = {} end
+if not lrm then lrm = {} end 
+if not lrm.defines then lrm.defines = {} end
 
-lrm.id = "logistic-request-manager"
-lrm.guiprefix = lrm.id .. "-gui-"
+lrm.defines.id        = "logistic-request-manager"
+lrm.defines.guiprefix = lrm.defines.id .. "-gui-"
 
-lrm.gui = {
-	toggle_button =     lrm.guiprefix .. "button",
+lrm.defines.gui = {
+	toggle_button               = lrm.defines.guiprefix .. "button",
 
-	master =            lrm.guiprefix .. "master",
-	frame =             lrm.guiprefix .. "frame",
-	flow =              lrm.guiprefix .. "flow",
+	master                      = lrm.defines.guiprefix .. "master",
+	frame                       = lrm.defines.guiprefix .. "frame",
+	flow                        = lrm.defines.guiprefix .. "flow",
 	
-	title_flow =        lrm.guiprefix .. "title_flow",
-	title_frame =		lrm.guiprefix .. "title_frame",
-	close_button =  	lrm.guiprefix .. "close_button",
-	test_button =  	lrm.guiprefix .. "test_button",
+	title_flow                  = lrm.defines.guiprefix .. "title_flow",
+	title_frame                 = lrm.defines.guiprefix .. "title_frame",
+	close_button                = lrm.defines.guiprefix .. "close_button",
+	test_button                 = lrm.defines.guiprefix .. "test_button",
 
-	toolbar =           lrm.guiprefix .. "toolbar",
-	save_as_textfield = lrm.guiprefix .. "save-as-input",
-	save_as_button =    lrm.guiprefix .. "save-as-button",
-	blueprint_button =  lrm.guiprefix .. "blueprint-request",
+	toolbar                     = lrm.defines.guiprefix .. "toolbar",
+	save_as_textfield           = lrm.defines.guiprefix .. "save-as-input",
+	save_as_button              = lrm.defines.guiprefix .. "save-as-button",
+	blueprint_button            = lrm.defines.guiprefix .. "blueprint-request",
 	
-	body =              lrm.guiprefix .. "body",
-	body_right =        lrm.guiprefix .. "body_right",
+	body                        = lrm.defines.guiprefix .. "body",
+	body_right                  = lrm.defines.guiprefix .. "body_right",
 	
-	sidebar =           lrm.guiprefix .. "sidebar",
-	sidebar_button =    lrm.guiprefix .. "sidebar-button",
-	save_button =       lrm.guiprefix .. "save-button",
-	load_button =       lrm.guiprefix .. "load-button",
-	delete_button =     lrm.guiprefix .. "delete-button",
-	export_button =     lrm.guiprefix .. "export-button",
-	import_button =     lrm.guiprefix .. "import-button",
+	sidebar                     = lrm.defines.guiprefix .. "sidebar",
+	sidebar_butto               = lrm.defines.guiprefix .. "sidebar-button",
+	save_button                 = lrm.defines.guiprefix .. "save-button",
+	load_button                 = lrm.defines.guiprefix .. "load-button",
+	delete_button               = lrm.defines.guiprefix .. "delete-button",
+	export_button               = lrm.defines.guiprefix .. "export-button",
+	import_button               = lrm.defines.guiprefix .. "import-button",
 	
-	target_menu =       lrm.guiprefix .. "target-menu",
-	target_label =      lrm.guiprefix .. "target-label",
-	target_slot =       lrm.guiprefix .. "target-slot",
+	target_menu                 = lrm.defines.guiprefix .. "target-menu",
+	target_label                = lrm.defines.guiprefix .. "target-label",
+	target_slot                 = lrm.defines.guiprefix .. "target-slot",
 	
-	preset_list =       lrm.guiprefix .. "preset-list",
-	preset_button =     lrm.guiprefix .. "preset-button",
-	preset_button_selected = lrm.guiprefix .. "preset-button-selected",
+	preset_list                 = lrm.defines.guiprefix .. "preset-list",
+	preset_button               = lrm.defines.guiprefix .. "preset-button",
+	preset_button_selected      = lrm.defines.guiprefix .. "preset-button-selected",
 	
-	request_window =    lrm.guiprefix .. "request-window",
-	request_table =     lrm.guiprefix .. "request-table",
-	request_slot =      lrm.guiprefix .. "request-slot",
-	request_min = 		lrm.guiprefix .. "request-min",
-	request_max = 	    lrm.guiprefix .. "request-max",
-	request_infinit =   lrm.guiprefix .. "request-infinit",
+	request_window              = lrm.defines.guiprefix .. "request-window",
+	request_table               = lrm.defines.guiprefix .. "request-table",
+	request_slot                = lrm.defines.guiprefix .. "request-slot",
+	request_min                 = lrm.defines.guiprefix .. "request-min",
+	request_max                 = lrm.defines.guiprefix .. "request-max",
+	request_infinit             = lrm.defines.guiprefix .. "request-infinit",
 
-	export_frame =		lrm.guiprefix .. "export-frame",
-	import_frame =		lrm.guiprefix .. "import-frame",
+	export_frame                = lrm.defines.guiprefix .. "export-frame",
+	import_frame                = lrm.defines.guiprefix .. "import-frame",
 
-	code_textbox =		lrm.guiprefix .. "code-textbox",
-	import_textfield =	lrm.guiprefix .. "import-textfield",
+	code_textbox                = lrm.defines.guiprefix .. "code-textbox",
+	import_textfield            = lrm.defines.guiprefix .. "import-textfield",
 
-	OK_button 	=		lrm.guiprefix .. "OK-button",
-	copy_button =		lrm.guiprefix .. "copy-button",
-	empty		=		lrm.guiprefix .. "empty",
+	OK_button                   = lrm.defines.guiprefix .. "OK-button",
+	copy_button                 = lrm.defines.guiprefix .. "copy-button",
+	empty                       = lrm.defines.guiprefix .. "empty",
 
-	import_preview_frame    	= lrm.guiprefix .. "import-preview-frame",
-	import_preview_toolbar		= lrm.guiprefix .. "import-preview-toolbar",
-	save_import_as_textfield	= lrm.guiprefix .. "save-import-as-textfield",
-	save_import_as_button		= lrm.guiprefix .. "save-import-as-button",
-	
+	import_preview_frame        = lrm.defines.guiprefix .. "import-preview-frame",
+	import_preview_toolbar      = lrm.defines.guiprefix .. "import-preview-toolbar",
+	save_import_as_textfield    = lrm.defines.guiprefix .. "save-import-as-textfield",
+	save_import_as_button       = lrm.defines.guiprefix .. "save-import-as-button",
 }
